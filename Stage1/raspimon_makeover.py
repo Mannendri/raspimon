34056af661105ea6d0905760ec8696e97b973ec6
Mannendri Olivares <molivares2022@columbiasecondary.org>
2021-03-06 11:15:39 -0500

    Completed Lab 3: Raspimon Makeover

--- Stage1/raspimon_makeover.py
+++ Stage1/raspimon_makeover.py
@@ -20,6 +20,9 @@ k = (0, 0, 0) # black means zero amounts of red, green and blue
 #define another color. Use one letter variable names to make it easy later
 b = (0,0,255)
 g = (0,255,0)
+w = (255,255,255)
+o = (255,128,0)
+y = (255,255,0)
 
 raspimon = [
 k, k, k, k, k, k, k, k,
@@ -36,6 +39,26 @@ sense.set_pixels(raspimon)
 
 #add a one-pixel mouth
 sense.set_pixel(3,4,r)
+sleep(2)
+
+pikachu = [
+w, k, k, w, w, w, w, k,
+w, w, y, o, w, w, w, o,
+w, w, w, y, y, y, y, o,
+o, o, w, y, k, y, y, k,
+o, o, w, r, y, y, y, o,
+w, o, w, y, o, o, o, w,
+w, o, y, o, y, o, y, w,
+w, w, y, o, o, o, o, w
+]
+sense.set_pixels(pikachu)
+	
+	
+	
+	
+	
+	
+	
  
 
 
